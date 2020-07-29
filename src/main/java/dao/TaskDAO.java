3c074bacd76583a9a0c2e4c63d01e517bf6dcb69
aleonov02 <aleonov02@yandex.ru>
2020-07-30 01:22:21 +0300

    Добавил методы для работы с базой данных

---
+++ src/main/java/dao/TaskDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import model.Task;
+
+import java.util.Date;
+import java.util.List;
+
+public interface TaskDAO {
+
+    void add(Task task);
+    void delete(Long number);
+    void update(Task task);
+    List<Task> get(Date fromDate, Date toDate);
+    List<Task> get(Date fromDate, Date toDate, Long number);
+    Task getByNumber(Long number);
+
+}
